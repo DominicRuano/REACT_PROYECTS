4480f5459af5e44936cc12017e89cc984eedbed9
DominicRuano <dojuparupe1975@gmail.com>
2024-11-13 17:24:51 -0600

    end 03

--- Proyects/03-mouse-follower/src/App.jsx
+++ Proyects/03-mouse-follower/src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react"
+import { Circle } from "./components/circle"
 
 function App() {
+  const [enable, setEnable] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enable) {
+      window.addEventListener('mousemove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('mousemove', handleMove)
+    }
+  }, [enable])
 
   return (
-    <>
-    <h3>proyecto 3</h3>
-    </>
+    <main>
+      <Circle position={position} />
+    <button onClick={() => {setEnable(!enable)}}>
+      { enable ? 'Desactivar' : 'Activar'}
+    </button>
+    </main>
   )
 }
 
---
+++ Proyects/03-mouse-follower/src/components/circle.jsx
@@ -0,0 +1,18 @@
+
+export function Circle ({ position }){
+    return (
+        <div style={{
+            position: "absolute",
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            border: '1px solid #fff',
+            borderRadius: '50%',
+            opacity: 0.8,
+            pointerEvents: 'none',
+            left: -20,
+            top: -20,
+            width: 40,
+            height: 40,
+            transform: `translate(${position.x}px, ${position.y}px)`,
+          }}/>
+    )
+}
